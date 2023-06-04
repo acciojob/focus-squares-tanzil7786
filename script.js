07efd31a5e751dc790d3990e8da487dd24c81951
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-04 16:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function changeColor(id){
+	for(let i=1;i<=3;i++){
+		if (i !== id) {
+			// Change its color
+			document.getElementById("square" + i).style.backgroundColor = "#6F4E37"; //coffee
+		}
+	}
+}
+function resetColor(id){
+	for(let i=1;i<=3;i++){
+		if (i !== id) {
+			document.getElementById("square" + i).style.backgroundColor = "#E6E6FA"; //lavender
+		}
+	}
+}
